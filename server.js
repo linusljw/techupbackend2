77a4e67a0ec5017fafca01a094b666a2a96c2d94
linusljw <linusljw@gmail.com>
2025-05-14 03:24:45 +0000

    Finished Section D: Activity 4

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 // Needed for Express
 var express = require('express')
+var axios = require('axios');
 var app = express()
 
 // Needed for EJS
@@ -95,6 +96,21 @@ app.post("/delete/:id", async (req, res) => {
     } catch (error) {
         console.log(error);
         res.redirect('/');
+    }
+  });
+
+
+  app.get('/demo', function(req, res) {
+  res.render('pages/demo');
+});
+
+app.get('/weather', async (req, res) => {
+    try {
+      const response = await axios.get('https://api-open.data.gov.sg/v2/real-time/api/twenty-four-hr-forecast');
+      res.render('pages/weather', { weather: response.data });
+    } catch (error) {
+      console.error(error);
+      res.send('Error fetching weather data');
     }
   });
 
